3900d72835051134485cfa84c784acce8aa586e1
jousethen <jthen93@gmail.com>
2021-08-20 13:09:25 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 15;
+let num4 = 11;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(12, 3, 4, 5, 20);
